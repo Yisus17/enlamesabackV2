b76c9e79ff77ef26ff5f651b9cc6c9fdef9ff0dd
Jesus Arevalo <jarevalo@moddo.com>
2019-08-07 21:23:18 +0200

    Config Java

---
+++ enlamesa/src/main/java/com/enlamesa/back/EnlamesaApplication.java
@@ -0,0 +1,13 @@
+package com.enlamesa.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnlamesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnlamesaApplication.class, args);
+	}
+
+}
